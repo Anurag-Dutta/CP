5265c9568588def228387588cc3ee014aebd6cdd
BlueKnight <1anuragdutta@gmail.com>
2022-07-06 00:34:12 +0530

    2022-07-06 00:33:54

---
+++ CSES/IntroductoryProblems/Number Spiral.cpp
@@ -0,0 +1,212 @@
+/* https://cses.fi/problemset/task/1071 */
+/*
+    AUTHOR : BlueKnight
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+// #include <boost/multiprecision/cpp_int.hpp> //Boost Library in C++
+// using namespace boost::multiprecision;
+using namespace std;
+#define SCD(t) scanf("%d", &t)
+#define SCLD(t) scanf("%ld", &t)
+#define SCLLD(t) scanf("%lld", &t)
+#define SCC(t) scanf("%c", &t)
+#define SCS(t) scanf("%s", t)
+#define SCF(t) scanf("%f", &t)
+#define SCLF(t) scanf("%lf", &t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+#define min_three(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
+#define max_three(a, b, c) ((a) > (b) ? ((a) > (c) ? (a) : (c)) : ((b) > (c) ? (b) : (c)))
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define FAST                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0); // To fasten the process
+#define read(type) readInt<type>()
+const double pi = acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+template <typename T>
+inline void write(T x)
+{
+    int i = 20;
+    char buf[21];
+    // buf[10] = 0;
+    buf[20] = '\n';
+
+    do
+    {
+        buf[--i] = x % 10 + '0';
+        x /= 10;
+    } while (x);
+    do
+    {
+        putchar(buf[i]);
+    } while (buf[i++] != '\n');
+}
+template <typename T>
+inline T readInt()
+{
+    T n = 0, s = 1;
+    char p = getchar();
+    if (p == '-')
+        s = -1;
+    while ((p < '0' || p > '9') && p != EOF && p != '-')
+        p = getchar();
+    if (p == '-')
+        s = -1, p = getchar();
+    while (p >= '0' && p <= '9')
+    {
+        n = (n << 3) + (n << 1) + (p - '0');
+        p = getchar();
+    }
+
+    return n * s;
+}
+int ispalindrome(int n)
+{
+    int a = 0;
+    int b = n;
+    while (n > 0)
+    {
+        a = a * 10 + (n % 10);
+        n = n / 10;
+    }
+    if (a == b)
+        return 1;
+    else
+        return 0;
+}
+bool isPrime(int number)
+{
+    if (number < 2)
+        return false;
+    if (number == 2)
+        return true;
+    if (number % 2 == 0)
+        return false;
+    for (int i = 3; (i * i) <= number; i += 2)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+int reverse(int n)
+{
+    int rem, rev = 0;
+    while (n > 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+    return rev;
+}
+double comb_l(double n, double k)
+{
+    return std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1));
+}
+int64 CEIL(int64 n, int64 k)
+{
+    if (n % k == 0)
+    {
+        return (n / k);
+    }
+    else
+    {
+        return (n / k) + 1;
+    }
+}
+int fact(int n);
+
+int nCr(int n, int r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+int fact(int n)
+{
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+int main()
+{
+    FAST int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        int64 x, y;
+        cin >> x >> y;
+        if (max(x, y) == x)
+        {
+            if (x % 2)
+            {
+                cout << x * x - 2 * (x - 1) + y - 1 << endl;
+            }
+            else
+            {
+                cout << x * x - (y - 1) << endl;
+            }
+        }
+        else
+        {
+            if (y % 2)
+            {
+                cout << y * y - (x - 1) << endl;
+            }
+            else
+            {
+                cout << y * y - 2 * (y - 1) + x - 1 << endl;
+            }
+        }
+    }
+    return 0;
+}
